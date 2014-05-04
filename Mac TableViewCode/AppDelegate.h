08d0b3c182d76922deb956ae73352bf6662f5d79
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-05-04 09:49:13 -0400

    Initial Commit

---
+++ Mac TableViewCode/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mac TableViewCode
+//
+//  Created by Jay Versluis on 04/05/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Mac TableViewCode/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  Mac TableViewCode
+//
+//  Created by Jay Versluis on 04/05/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
